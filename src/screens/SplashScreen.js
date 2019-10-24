369092ba8a61b09a34ea4f57ff848753776776ac
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-10-24 16:20:10 +0700

    redesign splashscreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,20 +1,35 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View, ActivityIndicator, Text} from 'react-native';
+import {View, ActivityIndicator, Text, StatusBar} from 'react-native';
 
 class SplashScreen extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.props.navigation.navigate('Home');
-    }, 2000);
+    }, 1000);
   }
 
   render() {
     return (
       <>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text style={{fontSize: 32, margin: 32}}>TransBatam</Text>
-          <ActivityIndicator color="blue" size={32} />
+        <StatusBar barStyle="light-content" backgroundColor="#072F5F" />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#072F5F',
+          }}>
+          <Text
+            style={{
+              fontSize: 32,
+              margin: 32,
+              color: 'white',
+              fontWeight: 'bold',
+            }}>
+            TransBatam
+          </Text>
+          <ActivityIndicator color="yellow" size={32} />
         </View>
       </>
     );
